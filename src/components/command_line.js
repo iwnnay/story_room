55e88f0b81c38d50e5db11ab2b07ece8965cbc02
Joe Imhoff <iwnnay@gmail.com>
2019-01-12 19:35:11 +0000

    Made the command line fit in better

--- src/components/command_line.js
+++ src/components/command_line.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const CommandLine = () => <input />;
+export default class CommandLine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
 
-export default CommandLine;
+  componentDidMount() {
+    this.input.focus();
+  }
+
+  handleBlur() {
+    this.input.focus();
+  }
+
+  render() {
+    return (
+      <input
+        id="command_line"
+        ref={(input) => { this.input = input; }}
+        name="command_line"
+        onBlur={this.handleBlur}
+      />
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './components/game';
 
-import css from './scss/global.scss';
+import './scss/global.scss';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
