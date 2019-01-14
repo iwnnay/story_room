4f62ef6e23a2f458c3b5d3e8ed93cd2febfed87c
Joe Imhoff <iwnnay@gmail.com>
2019-01-14 03:52:51 +0000

    Configure game loader and remove idea of overview

--- src/components/game.js
+++ src/components/game.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import GameService from '../engine/game';
+import LocalIO from '../engine/data_providers/local';
 import CommandLine from './command_line';
-import Overview from './overview';
 import Interaction from './interaction';
 
 export default class Game extends Component {
   constructor(props) {
     super(props);
-    this.game = new GameService();
+    this.game = new GameService(new LocalIO(), 'demo');
     this.commands = [];
 
     this.handleInput = this.handleInput.bind(this);
@@ -21,8 +21,7 @@ export default class Game extends Component {
   render() {
     return [
       <CommandLine key="CL" input={this.handleInput} />,
-      <Overview key="OV" info={this.game.getRoomDescription()} />,
-      <Interaction key="IA" info={this.game.getSituationDescription()} />,
+      <Interaction key="IA" info={this.game.situation.describe()} />,
     ];
   }
 }
