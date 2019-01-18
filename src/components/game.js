ecf3fbf9d4d2425ace767fe6e8553bfac5a8c048
Joe Imhoff <iwnnay@gmail.com>
2019-01-18 14:03:34 +0000

    GameEngine imported whole

--- src/components/game.js
+++ src/components/game.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import * as Story from 'text-story-game-engine';
+import GameEngine from 'text-story-game-engine';
 import CommandLine from './command_line';
 import Interaction from './interaction';
 import locations from '../data/locations';
 
-console.log(Story);
-const LocalIO = Story.Game.dataProviders.Local;
+const LocalIO = GameEngine.dataProviders.Local;
 
 export default class Game extends Component {
   constructor(props) {
     super(props);
-    this.game = new Story.Game.Engine(new LocalIO({ locations }), 'demo');
+    this.game = new GameEngine(new LocalIO({ locations }), 'demo');
     this.commands = [];
 
     this.handleInput = this.handleInput.bind(this);
